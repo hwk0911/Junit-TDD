d11ce03615643694ea66236a36f471a36d1ae595
hwk0911@gmail.com <hwk0911@gmail.com>
2020-01-25 17:52:04 +0900

    Programmers_Stack_6 주식가격 TEST 수정

--- test/Programmers_Stack_6Test.java
+++ test/Programmers_Stack_6Test.java
@@ -3,23 +3,23 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class Programmers_Stack_6Test {
-
     @Test
-    void solution() {
-        Programmers_Stack_6Test ps6 = new Programmers_Stack_6Test();
+    void testSolution() {
+        Programmers_Stack_6 ps6 = new Programmers_Stack_6();
+
         int[] prices = {1,2,3,2,3};
         int[] answer = {4,3,1,1,0};
 
-        assertEquals(true, answer.equals(ps6.solution(prices)));
+        assertEquals(answer, ps6.solution(prices));
     }
 
     @Test
     void getAnswer() {
-        Programmers_Stack_6Test ps6 = new Programmers_Stack_6Test();
+        Programmers_Stack_6 ps6 = new Programmers_Stack_6();
 
         int[] prices = {1,2,3,2,3};
         int[] answer = {4,3,1,1,0};
 
-        assertEquals(true, answer.equals(ps6.getAnswer(prices)));
+        assertEquals(answer, ps6.GetAnswer(prices));
     }
 }
