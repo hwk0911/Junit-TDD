5e877670547dbae903619fab595c24a76fb1544a
hwk0911@gmail.com <hwk0911@gmail.com>
2020-02-14 16:57:37 +0900

    Programmers_DFS_BFS_2 단어 변환
    answer 최소값 구하는 방법 다시 생각할것

--- src/Progreammers_DFS_BFS_2.java
+++ src/Progreammers_DFS_BFS_2.java
@@ -1,33 +1,115 @@
-public class Progreammers_DFS_BFS_2 {
-    public int solution(int n, int[][] computers) {
-        int answer = 0;
-        boolean[] checkNode = new boolean[n];
+import java.util.ArrayList;
+import java.util.Collections;
 
-        for(int index = 0 ; index < n ; index++){
-            checkNode[index] = false;
+public class Progreammers_DFS_BFS_2 {
+    static ArrayList<Integer> answerList = new ArrayList<>();
+
+    class StringNode {
+        int nodeNum;
+        String word;
+        ArrayList<StringNode> linkedNode = new ArrayList<>();
+
+        StringNode(int nodeNum, String word) {
+            this.nodeNum = nodeNum;
+            this.word = word;
         }
 
-        for(int index = 0; index < n ; index++){
-            if(checkNode[index] == false){
-                ++answer;
-                CheckNode(computers, checkNode, index);
+        private void addLinkedNode(StringNode stringNode) {
+            this.linkedNode.add(stringNode);
+        }
+
+        private String getWord() {
+            return this.word;
+        }
+
+        private int getListSize() {
+            return this.linkedNode.size();
+        }
+    }
+
+    public int solution(String begin, String target, String[] words) {
+        int answer = 0;
+        boolean[] visited = new boolean[words.length];
+
+        if(hasWord(words, target)){
+            ArrayList<StringNode> nodeList= setStringNode(begin, words);
+            StringNode head = linkStringNode(nodeList);
+
+            DFS(head, visited, 0, target);
+
+            Collections.sort(answerList);
+
+            answer = answerList.get(0);
+        }
+
+        return answer;
+    }
+
+    public boolean checkDifferent(String string, String target) {
+        int count = 0;
+
+        for (int index = 0, size = string.length(); index < size; ++index) {
+            if (string.charAt(index) != target.charAt(index)) {
+                ++count;
+            }
+        }
+
+        if (count != 0) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public ArrayList<StringNode> setStringNode(String begin, String[] words) {
+        ArrayList<StringNode> stringNodeArrayList = new ArrayList<>();
+
+        stringNodeArrayList.add(new StringNode(0, begin));
+
+        for (int index = 0, size = words.length; index < size; ++index) {
+            stringNodeArrayList.add(new StringNode(index + 1, words[index]));
+        }
+
+        return stringNodeArrayList;
+    }
+
+    public StringNode linkStringNode(ArrayList<StringNode> stringNodeArrayList) {
+        for (int index = 0, size = stringNodeArrayList.size(); index < size; ++index) {
+            for (int loop = 0; loop < size; ++loop) {
+                if (checkDifferent(stringNodeArrayList.get(index).getWord(), stringNodeArrayList.get(loop).getWord())) {
+                    stringNodeArrayList.get(index).addLinkedNode(stringNodeArrayList.get(loop));
+                }
+            }
+        }
+
+        return stringNodeArrayList.get(0);
+    }
+
+    public int DFS(StringNode startNode, boolean[] visited, int answer, String target) {
+        visited[startNode.nodeNum] = true;
+
+        if (startNode.getWord().equals(target)) {
+            return answer;
+        }
+
+        for (int index = 0, size = startNode.getListSize(); index < size; ++index) {
+            if (!visited[startNode.linkedNode.get(index).nodeNum]) {
+                answer = DFS(startNode.linkedNode.get(index), visited, answer + 1, target);
+                answerList.add(answer);
+                answer = 0;
             }
         }
 
         return answer;
     }
 
-    public boolean[] CheckNode (int[][] computers, boolean[] checkNode, int node){
-        checkNode[node] = true;
-
-        for(int index = 0, size = computers.length ; index < size ; index++){
-            if(computers[node][index] == 1){
-                if(checkNode[index] == false){
-                    CheckNode(computers, checkNode, index);
-                }
+    public boolean hasWord (String[] words, String target){
+        for(int index = 0, size = words.length ; index < size ; ++index){
+            if(target.equals(words[index])){
+                return true;
             }
         }
 
-        return checkNode;
+        return false;
     }
 }
