28f6f6662e9d5b335722ab40d21d9070215d6a34
hwk0911@gmail.com <hwk0911@gmail.com>
2020-02-19 21:28:12 +0900

    BOJ_Function_15596_정수N개의합 test

---
+++ test/BOJ_Function_15596_정수N개의합Test.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BOJ_Function_15596_정수N개의합Test {
+
+    @Test
+    void sum() {
+        BOJ_Function_15596_정수N개의합 bf = new BOJ_Function_15596_정수N개의합();
+
+        int[] a = {1,2,3,4,5};
+
+        assertEquals(15, bf.sum(a));
+    }
+}
