319491d72a7ee7c51c9e4aba794339e4f391f5a4
hwk0911@gmail.com <hwk0911@gmail.com>
2020-02-20 20:07:20 +0900

    Programmers_Sort_2Test.java

--- test/Programmers_Sort_2Test.java
+++ test/Programmers_Sort_2Test.java
@@ -9,22 +9,18 @@ class Programmers_Sort_2Test {
         Programmers_Sort_2 ps2 = new Programmers_Sort_2();
 
         int numbers[] = {6, 10, 2};
+        String answer = "6210";
 
-
-        if("6210".equals(ps2.solution(numbers))){
-            System.out.println("true");
-        }
-        else{
-            System.out.println("false" + ps2.solution(numbers));
-        }
+        assertEquals(answer, ps2.solution(numbers).intern());
 
         int numbers_2[] = {3,30,34,5,9};
+        answer = "9534330";
 
-        if("9534330".equals(ps2.solution(numbers_2))){
-            System.out.println("true");
-        }
-        else{
-            System.out.println("false" + ps2.solution(numbers_2));
-        }
+        assertEquals(answer, ps2.solution(numbers_2).intern());
+
+        int numbers_3[] = {0,000,0000,0000000,0000000};
+        answer = "0";
+
+        assertEquals(answer, ps2.solution(numbers_3).intern());
     }
 }
