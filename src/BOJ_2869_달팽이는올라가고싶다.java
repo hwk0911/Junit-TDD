0d65f4882ccf7b6922ed6adb9f51ac830476fb64
hwk0911@gmail.com <hwk0911@gmail.com>
2020-02-24 18:17:32 +0900

    BOJ_2869_달팽이는올라가고싶다.java

---
+++ src/BOJ_2869_달팽이는올라가고싶다.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2869_달팽이는올라가고싶다 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] str;
+        str = br.readLine().split(" ");
+
+        int A = Integer.parseInt(str[0]);
+        int B = Integer.parseInt(str[1]);
+        int V = Integer.parseInt(str[2]);
+
+        V -= A;
+        A -= B;
+
+        if(V % A == 0){
+            System.out.println((V / A) + 1);
+        }
+        else{
+            System.out.println((V / A) + 2);
+        }
+    }
+}
