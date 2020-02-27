c28137bcff00a38d95d66584f462511fa9faa765
hwk0911@gmail.com <hwk0911@gmail.com>
2020-02-27 19:07:32 +0900

    BOJ_Math1_2869_달팽이는올라가고싶다.java
    이름에 알고리즘 분류 추가

--- src/BOJ_Math1_2869_달팽이는올라가고싶다.java
+++ src/BOJ_Math1_2869_달팽이는올라가고싶다.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class BOJ_2869_달팽이는올라가고싶다 {
+public class BOJ_Math1_2869_달팽이는올라가고싶다 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
