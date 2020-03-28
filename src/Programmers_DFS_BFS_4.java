f0412daa8dc832680a57e53fbb0fe02eeb51c7e5
hwk0911@gmail.com <hwk0911@gmail.com>
2020-03-28 19:32:49 +0900

    Programmers_DFS_BFS_4.java
    setRoute 함수 에러
    
    1. 그래프 그려서 해결해볼것
    2. 출력 하나 부족한 이유 찾을것.

--- src/Programmers_DFS_BFS_4.java
+++ src/Programmers_DFS_BFS_4.java
@@ -1,7 +1,3 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class Programmers_DFS_BFS_4 {
     String route;
 
@@ -12,29 +8,30 @@ public class Programmers_DFS_BFS_4 {
         visited[0] = true;
 
 
+        setRoute(tickets, visited, tickets[0][0], tickets[0][1], 1);
+
         answer = route.split(" ");
         return answer;
     }
 
-    public void setRoute (String[][] tickets, boolean[] visited, String tempRoute, String destination, int usingCount) {
-        if(usingCount == tickets.length) {
+    public void setRoute(String[][] tickets, boolean[] visited, String tempRoute, String destination, int usingCount) {
+        if (usingCount == tickets.length) {
             this.route = tempRoute;
             return;
         }
 
-        for(int index = 1, size = tickets.length ; index < size ; ++index) {
-            if(!visited[index] && checkEqaul(destination, tickets[index][0])) {
+        for (int index = 1, size = tickets.length; index < size; ++index) {
+            if (!visited[index] && checkEqaul(destination, tickets[index][0])) {
                 visited[index] = !visited[index];
                 setRoute(tickets, visited, tempRoute + " " + destination, tickets[index][1], usingCount + 1);
             }
         }
     }
 
-    public boolean checkEqaul (String str1, String str2) {
-        if(str1.equals(str2)) {
+    public boolean checkEqaul(String str1, String str2) {
+        if (str1.equals(str2)) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
