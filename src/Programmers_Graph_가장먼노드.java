64030af2bc7be8a69f39f5d36be2aedbbcaec091
hwk0911@gmail.com <hwk0911@gmail.com>
2020-04-03 19:07:58 +0900

    Programmers_Graph_가장먼노드.java
    성공

--- src/Programmers_Graph_가장먼노드.java
+++ src/Programmers_Graph_가장먼노드.java
@@ -9,61 +9,65 @@ public class Programmers_Graph_가장먼노드 {
     public int solution(int n, int[][] edge) {
         int answer = 0;
 
-        List<List<Integer>> link = setLinkNode(n, edge);
-        visited = new boolean[n];
-        visited[0] = true;
+        this.visited = new boolean[n];
+        List<List<Integer>> nodeList = setList(n, edge);
+        Queue<Integer> nodeQue = new LinkedList<>();
+        nodeQue.offer(0);
+        this.visited[0] = true;
 
-        Queue<Integer> que = new LinkedList<>();
-        que.offer(0);
-
-        do {
-            answer = que.size();
-            que = setQueue(que, link);
-        }while (!que.isEmpty());
+        while(!nodeQue.isEmpty()) {
+            answer = nodeQue.size();
+            nodeQue = setQue(nodeQue, nodeList);
+        }
 
         return answer;
     }
 
-    public List<List<Integer>> setLinkNode (int n, int[][] edge) {
-        List<List<Integer>> link = new ArrayList<>();
+    public List<List<Integer>> setList (int n, int[][] edge) {
+        List<List<Integer>> nodeList = new ArrayList<>();
         List<Integer> temp;
 
-        for(int index = 0 ; index < n ; ++index) {
-            link.add(new ArrayList<>());
+        for(int index = 0, size = n ; index < size ; ++index) {
+            nodeList.add(new ArrayList<>());
         }
 
         for(int index = 0, size = edge.length ; index < size ; ++index) {
-            temp = link.get(edge[index][0]);
-            temp.add(edge[index][1] - 1);
-            link.set(edge[index][0], temp);
-        }
+            int start = edge[index][0] - 1;
+            int end = edge[index][1] - 1;
 
-        for(int index = 0, size = link.size() ; index < size ; ++index) {
-            temp = link.get(index);
-            System.out.println(index+1 + " 연결");
-            for(int i : temp) {
-                System.out.println(i + 1);
+            temp = nodeList.get(start);
+
+            if(!temp.contains(end)) {
+                temp.add(end);
+            }
+
+            temp = nodeList.get(end);
+
+            if(!temp.contains(start)) {
+                temp.add(start);
             }
         }
 
-        return link;
+        return nodeList;
     }
 
-    public Queue<Integer> setQueue (Queue<Integer> node, List<List<Integer>> link) {
-        Queue<Integer> que = new LinkedList<>();
+    public Queue<Integer> setQue (Queue<Integer> queue, List<List<Integer>> nodeList) {
         List<Integer> temp;
+        Queue<Integer> newQueue = new LinkedList<>();
 
-        for(int nodeNum : node) {
-            temp = link.get(nodeNum);
+        while(!queue.isEmpty()) {
+            temp = nodeList.get(queue.poll());
 
             for(int index = 0, size = temp.size() ; index < size ; ++index) {
-                if(!visited[temp.get(index)]) {
-                    visited[temp.get(index)] = true;
-                    que.offer(temp.get(index));
+                int nodeLink = temp.get(index);
+
+                if(!this.visited[nodeLink]) {
+                    newQueue.offer(nodeLink);
+                    this.visited[nodeLink] = !this.visited[nodeLink];
                 }
             }
         }
 
-        return que;
+        return newQueue;
     }
 }
