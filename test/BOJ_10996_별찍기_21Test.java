2888823a9175ce693190de708381e9f4675e2cb4
hwk0911@gmail.com <hwk0911@gmail.com>
2020-04-06 13:48:00 +0900

    BOJ_10996_별찍기_21Test.java

---
+++ test/BOJ_10996_별찍기_21Test.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BOJ_10996_별찍기_21Test {
+
+    BOJ_10996_별찍기_21 main = new BOJ_10996_별찍기_21();
+
+    @Test
+    void pickStar() throws Exception{
+        main.pickStar(3, 0);
+    }
+}
