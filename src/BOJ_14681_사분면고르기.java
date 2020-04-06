f0ddc26c61d09f2cae74fd06c11b0ccf7aff0760
hwk0911@gmail.com <hwk0911@gmail.com>
2020-04-06 13:08:24 +0900

    BOJ_14681_사분면고르기.java

--- src/BOJ_14681_사분면고르기.java
+++ src/BOJ_14681_사분면고르기.java
@@ -1,31 +1,33 @@
-import java.io.*;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class BOJ_14681_사분면고르기 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int x = Integer.parseInt(br.readLine());
         int y = Integer.parseInt(br.readLine());
 
+        System.out.println(selectQuadrant(x, y));
+    }
+
+    public static int selectQuadrant (int x, int y) {
         if(x * y > 0) {
             if(x < 0) {
-                bw.write("3");
+                return 3;
             }
             else {
-                bw.write("1");
+                return 1;
             }
         }
         else {
             if(x < 0) {
-                bw.write("2");
+                return 2;
             }
             else {
-                bw.write("4");
+                return 4;
             }
         }
-
-        bw.flush();
-        bw.close();
     }
 }
